acc8417f50117924b32ebd953626dd044b3881d8
Gaury <gaury481@gmail.com>
2025-06-16 08:26:19 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Example using if statement
+age=20
+if age>=18:
+    print("you are an adult")
+    
+    #Example using if-else statement
+    number=7
+    if number %2 ==0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+
+        #Example using if-elif-else statement
+        score=85
+        if score>=90:
+            print("grade:A")
+        elif score>=80:
+            print("grade:B")
+        elif score>=70:
+            print("grade:C")
+        else:
+            print("grade:below C")
+
