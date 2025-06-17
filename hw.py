d7ce3bc6a660ca240e80387b85ec613034d5ec1f
Gaury <gaury481@gmail.com>
2025-06-17 11:10:25 +0530

    initial commit

---
+++ hw.py
@@ -0,0 +1,16 @@
+#program to check whether a number is positive
+value=1
+if value>=0:
+    print("The number is positive")
+#program to check whether if the number is divible by 5
+number=25
+if number %5==0:
+    print("The number is divisible")    
+#program to check whether if a given character is a vowel(a,e,i,o,u)
+value='a'
+if value =='a'or value =='e' or value == 'i' or value =='o' or value =='u':
+    print(value,"is a vowel")
+#program to check if the entered string is "python"    
+user_input= input("enter a string:")
+if user_input =='python':
+    print("you entered 'python'!")
--- main2.py
+++ main2.py
@@ -1,26 +1,3 @@
-# #Example using if statement
-# age=101
-# if age>=18:
-#     print("you are an adult")
-    
-#     #Example using if-else statement
-#     number=7
-#     if number %2 ==0:
-#         print("The number is even.")
-#     else:
-#         print("The number is odd.")
-
-#         #Example using if-elif-else statement
-#         score=-1
-#         if score>=90:
-#             print("grade:A")
-#         elif score>=80:
-#             print("grade:B")
-#         elif score>=70:
-#             print("grade:C")
-#         else:
-#             print("grade:below C")
-  
 i=20
 if(i<15):
     print("i is smaller than 15")
@@ -38,7 +15,7 @@ if(i<20):
         print("i is smaller than 12 too")
     else:
         print("i is greater than 15")
-ample for if elif else statement
+#example for if elif else statement
 i=20
 if(i==10):
     print("i is 10")
